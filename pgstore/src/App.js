a862a820f7a09c994741071109a6b0ef4f8a8684
drope <pstrauch@gmail.com>
2024-08-09 11:15:03 -0300

    Initial commit

---
+++ pgstore/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./Navbar.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <header className="App-header">Punta Goods</header>
+        <Routes>
+          <Route path="/" />
+          <Route path="/carrito" />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
