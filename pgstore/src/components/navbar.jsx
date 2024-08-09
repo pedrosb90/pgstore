fc720d595e0c95df01fd4015c4060519d7f3c2a0
drope <pstrauch@gmail.com>
2024-08-09 11:49:32 -0300

    navbar/ routes - cart icon

--- pgstore/src/components/navbar.jsx
+++ pgstore/src/components/navbar.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
 
 export const Navbar = () => {
-  return <div className="Nav-header">navbar</div>;
+  return (
+    <div className="Nav-header">
+      navbar
+      <div className="Links">
+        <Link to="/">Productos</Link>
+        <Link to="/carrito">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+  );
 };
