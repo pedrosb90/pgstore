dc0a21ab5aea3c068777fff5ef5fa25240e855fb
drope <pstrauch@gmail.com>
2024-08-27 13:23:25 -0300

    server/ added deployment url

--- pgstore/server/index.js
+++ pgstore/server/index.js
@@ -18,7 +18,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://puntagoodsstore-pedrosb90-s-team.vercel.app"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
