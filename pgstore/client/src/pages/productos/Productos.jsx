ff93cf49e877fc32395be0c38afc76dc7117eac5
drope <pstrauch@gmail.com>
2026-04-07 09:02:10 -0300

    info

--- pgstore/client/src/pages/productos/Productos.jsx
+++ pgstore/client/src/pages/productos/Productos.jsx
@@ -14,7 +14,10 @@ export const Productos = () => {
       <p className="info">
         Entrego los MIERCOLES en departamento de Maldonado.
       </p>
-      <p className="info">Pago por TRANSFERENCIA: Pedir por Whatsapp'</p>
+      <p className="info">
+        $$ TRANSFERENCIA $$: Armá tu carro y envia captura de pantalla por
+        Whatsapp.
+      </p>
       <div className="item-container">
         {shopitems.map((producto) => (
           <Item data={producto} />
